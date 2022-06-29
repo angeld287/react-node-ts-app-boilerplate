2f3b9b70afe8645be54a8a461a516366cdeae2c6
Angel Angeles <daniel_1234@hotmail.es>
2022-06-29 19:27:08 -0400

    performing test handlers

--- client/src/screens/Login/Handlers.ts
+++ client/src/screens/Login/Handlers.ts
@@ -1,32 +1,50 @@
-import { rest } from "msw";
+import { ResponseTransformer, rest } from "msw";
 
 const login = rest.post('http://localhost:3001/api/auth/login', (req, res, ctx) => {
-    return res(
+    const _error: any = [];
+    const _success: any = null;
+
+    const headers: Array<ResponseTransformer<any, any>> = [
         ctx.status(200),
         ctx.set("access-control-allow-origin", "*"),
         ctx.set("Accept", "application/json"),
-        ctx.set("Content-Type", "application/json"),
-        ctx.json({
-            statusCode: "10000",
-            message: "Success",
-            data: {
-                errors: [
-                    {
-                        message: "E-mail cannot be blank.",
-                        value: "",
-                        param: "username",
-                        location: "body"
-                    },
-                    {
-                        message: "E-mail is not valid.",
-                        value: "",
-                        param: "username",
-                        location: "body"
-                    }
-                ]
-            }
-        })
-        , ctx.delay(10)
+        ctx.set("Content-Type", "application/json")
+    ]
+
+    let json: ResponseTransformer<any, any> = ctx.json({})
+
+    if (!req.body)
+        return res(
+            ...headers,
+            json,
+            ctx.delay(100)
+        )
+
+    json = ctx.json({
+        statusCode: "10000",
+        message: "Success",
+        data: {
+            errors: [
+                {
+                    message: "E-mail cannot be blank.",
+                    value: "",
+                    param: "username",
+                    location: "body"
+                },
+                {
+                    message: "E-mail is not valid.",
+                    value: "",
+                    param: "username",
+                    location: "body"
+                }
+            ]
+        }
+    })
+
+    return res(
+        ...headers,
+        json,
+        ctx.delay(10)
     )
 });
 
